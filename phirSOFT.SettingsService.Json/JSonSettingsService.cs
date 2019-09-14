6abc28bbb3c460ace132ea0b394a2a3a8e774a49
Philemon Eichin <philemon.eichin@t4dt.com>
2019-09-14 20:38:17 +0200

    Cleanup Code

--- phirSOFT.SettingsService.Json/JSonSettingsService.cs
+++ phirSOFT.SettingsService.Json/JSonSettingsService.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿// <copyright file="JsonSettingsService.cs" company="phirSOFT">
+// Copyright (c) phirSOFT. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -11,7 +17,7 @@ using phirSOFT.SettingsService.Abstractions;
 namespace phirSOFT.SettingsService.Json
 {
     /// <summary>
-    /// Implements a <see cref="ISettingsService"/> for settings stored in json file.
+    ///     Implements a <see cref="ISettingsService"/> for settings stored in json file.
     /// </summary>
     public sealed class JsonSettingsService : CachedSettingsService
     {
@@ -20,8 +26,9 @@ namespace phirSOFT.SettingsService.Json
         private readonly IDictionary<string, Type> _types;
 
         private readonly IDictionary<string, object> _values;
+
         /// <summary>
-        /// Initializes a new instance of the <see cref="JsonSettingsService"/> class.
+        ///     Initializes a new instance of the <see cref="JsonSettingsService"/> class.
         /// </summary>
         /// <param name="filename">the filename.</param>
         private JsonSettingsService(string filename)
@@ -32,20 +39,23 @@ namespace phirSOFT.SettingsService.Json
             _defaultValues = new ConcurrentDictionary<string, object>();
         }
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         protected override bool SupportConcurrentRegister => false;
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         protected override bool SupportConcurrentUnregister => true;
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         protected override bool SupportConcurrentUpdate => false;
 
         /// <summary>
-        /// Creates a new <see cref="JsonSettingsService"/> asynchronously
+        ///     Creates a new <see cref="JsonSettingsService"/> asynchronously
         /// </summary>
         /// <param name="filename">the name of the settings file to open or create.</param>
-        /// <returns>A <see cref="Task"/>, that represents the asynchronous create operation and yields <see cref="JsonSettingsService"/></returns>
+        /// <returns>
+        ///     A <see cref="Task"/>, that represents the asynchronous create operation and yields
+        ///     <see cref="JsonSettingsService"/>
+        /// </returns>
         public static async Task<JsonSettingsService> CreateAsync(string filename)
         {
             var service = new JsonSettingsService(filename);
@@ -53,19 +63,19 @@ namespace phirSOFT.SettingsService.Json
             return service;
         }
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         protected override Task<object> GetSettingInternalAsync(string key, Type type)
         {
             return RunSynchronous(() => GetSettingInternal(key, type));
         }
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         protected override Task<bool> IsRegisteredInternalAsync(string key)
         {
             return Task.FromResult(_values.ContainsKey(key));
         }
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         protected override Task RegisterSettingInternalAsync(
             string key,
             object defaultValue,
@@ -75,13 +85,13 @@ namespace phirSOFT.SettingsService.Json
             return RunSynchronous(() => RegisterSettingsInternal(key, defaultValue, initialValue, type));
         }
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         protected override Task SetSettingInternalAsync(string key, object value, Type type)
         {
             return RunSynchronous(() => SetSettingsInternal(key, value, type));
         }
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         protected override async Task StoreInternalAsync()
         {
             var serializer = new JsonSerializer();
@@ -101,7 +111,7 @@ namespace phirSOFT.SettingsService.Json
             }
         }
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         protected override Task UnregisterSettingInternalAsync(string key)
         {
             return RunSynchronous(() => UnregisterSettingInternal(key));
@@ -177,7 +187,10 @@ namespace phirSOFT.SettingsService.Json
             _types.Add(key, type);
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "We rethrow the exception, by returning a failed tas.")]
+        [SuppressMessage(
+            "Design",
+            "CA1031:Do not catch general exception types",
+            Justification = "We rethrow the exception, by returning a failed tas.")]
         private static Task RunSynchronous(Action action)
         {
             try
@@ -210,9 +223,12 @@ namespace phirSOFT.SettingsService.Json
         private void SetSettingsInternal(string key, object value, Type type)
         {
             if (!value.GetType().GetTypeInfo().IsAssignableFrom(type.GetTypeInfo()))
-                throw new ArgumentException($"Value of type {value.GetType().FullName} did not match expected type {type.FullName}. ",nameof(value));
+                throw new ArgumentException(
+                    $"Value of type {value.GetType().FullName} did not match expected type {type.FullName}. ",
+                    nameof(value));
             if (type != _types[key])
-                throw new ArgumentException($"{type.FullName} was different, than stored property setting type ({_types[key].FullName})");
+                throw new ArgumentException(
+                    $"{type.FullName} was different, than stored property setting type ({_types[key].FullName})");
 
             _values[key] = value;
         }
